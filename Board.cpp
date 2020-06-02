a9d24817a3329a9e3dbf588d64d465275209c8a3
liorls <57623323+liorls@users.noreply.github.com>
2020-06-02 22:37:12 +0300

    board.cpp

--- Board.cpp
+++ Board.cpp
@@ -14,12 +14,55 @@ Soldier* Board::operator[](std::pair<int,int> location) const{
 }
 
 void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction){
+    Soldier* s = (*this)[source];
+    if(s == nullptr)
+        throw invalid_argument("No soldier at the source point");
+    if(s->numTeam != player_number)
+        throw invalid_argument("The move is illegal");
+    std::pair<int,int> step;
+    switch (direction)
+    {
+    case Up:
+        step.first = source.first + 1;
+        step.second = source.second;
+        break;
+    case Down:
+        step.first = source.first - 1;
+        step.second = source.second;
+        break;
+    case Left:
+        step.first = source.first;
+        step.second = source.second - 1;
+        break;
+    case Right:
+        step.first = source.first;
+        step.second = source.second + 1;
+        break;
+    }
+    
+    if(step.first >= board.size() || step.first < 0 || step.second >= board[0].size() || step.second < 0)
+        throw invalid_argument("Out of the board");
+    if((*this)[step] != nullptr)
+        throw invalid_argument("The move is illegal");
 
+    (*this)[step] = s;
+    (*this)[source] = nullptr;
 
+    s->attack(board,step);
 }
 
 bool Board::has_soldiers(uint player_number) const{
-
-    return true;
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[0].size(); j++)
+        {
+            Soldier* s = (*this)[{i,j}];
+            if(s != nullptr && s->numTeam == player_number)
+            return true;
+        }
+        
+    }
+    
+    return false;
 }
     
